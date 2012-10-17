6d6e4dea6611d43430e894fb285d59095c5f4564
Hector Castro <hectcastro@gmail.com>
2012-10-17 14:41:05 -0400

    Bumped Node.js version to v0.8.12.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["nodejs"]["version"]          = "0.6.13"
+default["nodejs"]["version"]          = "0.8.12"
 default["nodejs"]["dir"]              = "/usr/local"
-default["nodejs"]["url"]              = "http://nodejs.org/dist/node-v#{node["nodejs"]["version"]}.tar.gz"
-default["nodejs"]["checksum"]         = "6cf4311ecbc1700e88f4382a31b3a7017c1572cd641fd06e653fc1692c2cffff"
+default["nodejs"]["url"]              = "http://nodejs.org/dist/v#{node["nodejs"]["version"]}/node-v#{node["nodejs"]["version"]}.tar.gz"
+default["nodejs"]["checksum"]         = "d64a7f2ab8f4419a11bde5379d6065666fd1cc4593ec828cb5ac57385efafa45"
