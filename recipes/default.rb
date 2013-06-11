e0f379e68c911ed0cdcd3d812f24493c8f669b55
Hector Castro <hectcastro@gmail.com>
2013-06-11 12:31:41 -0400

    Fix FC043.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,18 +12,17 @@ node_packages.each do |node_package|
   end
 end
 
+remote_file "#{Chef::Config[:file_cache_path]}/node-v#{node["nodejs"]["version"]}.tar.gz" do
+  source node["nodejs"]["url"]
+  checksum node["nodejs"]["checksum"]
+end
+
 bash "install-node" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
     tar -xzf node-v#{node["nodejs"]["version"]}.tar.gz
     (cd node-v#{node["nodejs"]["version"]} && ./configure --prefix=#{node["nodejs"]["dir"]} && make && make install)
   EOH
-  action :nothing
+  action :run
   not_if "#{node["nodejs"]["dir"]}/bin/node --version 2>&1 | grep #{node["nodejs"]["version"]}"
-end
-
-remote_file "#{Chef::Config[:file_cache_path]}/node-v#{node["nodejs"]["version"]}.tar.gz" do
-  source node["nodejs"]["url"]
-  checksum node["nodejs"]["checksum"]
-  notifies :run, resources(:bash => "install-node"), :immediately
 end
