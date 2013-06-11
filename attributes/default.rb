de400bd0008a4d1a5307d28349b5fee577ea6282
Hector Castro <hectcastro@gmail.com>
2013-06-11 12:32:51 -0400

    Update Node.js to v0.10.10.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["nodejs"]["version"]          = "0.8.12"
+default["nodejs"]["version"]          = "0.10.10"
 default["nodejs"]["dir"]              = "/usr/local"
 default["nodejs"]["url"]              = "http://nodejs.org/dist/v#{node["nodejs"]["version"]}/node-v#{node["nodejs"]["version"]}.tar.gz"
-default["nodejs"]["checksum"]         = "d64a7f2ab8f4419a11bde5379d6065666fd1cc4593ec828cb5ac57385efafa45"
+default["nodejs"]["checksum"]         = "a54de71d2c3ac7ae864ab9640b6eecb27d7d49c190ac1ca6526243fd7a8ad15c"
